f0937bb4e15ef207cd63991a1defa062b6f56f24
DEZINA\dezina <dezinap.brainerhub@gmail.com>
2023-12-28 15:52:42 +0530

    applied cors fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
   const port = 4000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`)
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
